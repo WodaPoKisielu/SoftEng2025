81ae87024d56b6bcd420a65f2f8c4df652ee0e1f
Aleksander <gorskimarcinek@wp.pl>
2025-06-01 13:23:56 +0200

    formatted

--- src/tests/test_select.cpp
+++ src/tests/test_select.cpp
@@ -1,21 +1,33 @@
-
-#include <gtest/gtest.h>
-#include "Sorter.h"
 #include "SortData.h"
+#include "Sorter.h"
+#include <chrono>
+#include <gtest/gtest.h>
 
-TEST(test_select, AddFunction)
+TEST(test_select, MeasureSortTime)
 {
     Sorter* sorter = new Sorter();
     SortData<float, std::vector> data;
-
     int dsize = 128 + rand() % 100;
     for (unsigned i = 0; i < dsize; i++) data.add(float(rand() % 1024) / 4096);
-    auto error = sorter->sort<float, std::vector, STT_SELECT>(data);
-
+    auto start_unsorted = std::chrono::high_resolution_clock::now();
+    auto error_unsorted = sorter->sort<float, std::vector, STT_SELECT>(data);
+    auto end_unsorted = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> duration_unsorted =
+        end_unsorted - start_unsorted;
+    std::cout << "Sorting time (unsorted): " << duration_unsorted.count()
+              << " ms" << std::endl;
     ASSERT_TRUE(std::is_sorted(data.data().begin(), data.data().end()));
-    ASSERT_EQ(error, SE_SUCCESS);
+    ASSERT_EQ(error_unsorted, SE_SUCCESS);
+    auto start_sorted = std::chrono::high_resolution_clock::now();
+    auto error_sorted = sorter->sort<float, std::vector, STT_SELECT>(data);
+    auto end_sorted = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> duration_sorted =
+        end_sorted - start_sorted;
+    std::cout << "Sorting time (already sorted): " << duration_sorted.count()
+              << " ms" << std::endl;
+    ASSERT_TRUE(std::is_sorted(data.data().begin(), data.data().end()));
+    ASSERT_EQ(error_sorted, SE_SUCCESS);
 }
-
 
 int main(int argc, char** argv)
 {
